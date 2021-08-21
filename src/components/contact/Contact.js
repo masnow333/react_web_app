0f783a06fac64029de130b5eaa053c6ebda867f5
jonathan triana <jonfertri333@gmail.com>
2021-08-20 20:21:44 -0700

    The info in contact was added

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import './Contact.css'
 
 export const Contact = () => {
     return (
         <>
-            <h2>Contact</h2>   
+            <h2 className="contact">Contact me</h2> 
+            <p className="phone">Phone number: +57 3195413658</p>
+            <p className="mail">Mail: <a href="mailto:jonfertri357@gmail.com">jonfertri357@gmail.com</a></p>
         </>
     )
 }
