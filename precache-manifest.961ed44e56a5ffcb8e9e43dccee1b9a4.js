a7e0a34750b3e31fa0c0f1f383b6b8cfc742a2e7
masnow333 <jonfertri333@gmail.com>
2021-11-29 19:46:20 -0500

    Updates

---
+++ precache-manifest.961ed44e56a5ffcb8e9e43dccee1b9a4.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34adea07490da97e71484105d22a193a",
+    "url": "/react_web_app/index.html"
+  },
+  {
+    "revision": "7ac1abe2de9427157fab",
+    "url": "/react_web_app/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "abd13b4e6c34b0193432",
+    "url": "/react_web_app/static/css/main.61fa0d4d.chunk.css"
+  },
+  {
+    "revision": "7ac1abe2de9427157fab",
+    "url": "/react_web_app/static/js/2.7b4102b0.chunk.js"
+  },
+  {
+    "revision": "fb6fca4f0fa26a7e27d26480a74532c9",
+    "url": "/react_web_app/static/js/2.7b4102b0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "70b0810883ea7bf56294",
+    "url": "/react_web_app/static/js/3.24998ccf.chunk.js"
+  },
+  {
+    "revision": "abd13b4e6c34b0193432",
+    "url": "/react_web_app/static/js/main.16826c18.chunk.js"
+  },
+  {
+    "revision": "a03ae6ba07f874f3cc73",
+    "url": "/react_web_app/static/js/runtime-main.78d78f47.js"
+  }
+]);
